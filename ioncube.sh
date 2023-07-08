651c5d2895c6f2520ced2cdd48047a7d0f0d9327
Iqra Muskan <iqra.muskan@cloudways.com>
2023-07-08 22:16:53 +0000

    commit from different branch

---
+++ ioncube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /tmp
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip
+unzip ioncube_loaders_lin_x86-64.zip
+cd ioncube
+
---
+++ test.php4
@@ -0,0 +1,3 @@
+<?php echo var_export(extension_loaded('ionCube Loader') ,true);
+
+#PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
